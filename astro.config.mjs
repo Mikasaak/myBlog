aa2ec278ccd16d217729cea9e3ed57e10f2dd7c8
Yu Zhenghao <1474691736@qq.com>
2024-04-11 19:40:59 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mikasaak.github.io',
-	base: '/myblog',
+	// base: '/myblog',
+	// base: '/',
 	integrations: [mdx(), sitemap()],
 	server:{
 		port: 8888
